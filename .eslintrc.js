ca5ce541eda080207cd0ac10b66b27560fec4cc8
amasalov <delsol@bk.ru>
2018-12-09 16:09:25 +0300

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  env: {
+    node: true,
+  },
+  parserOptions: {
+    ecmaVersion: 8,
+    sourceType: 'module',
+  },
+  extends: ['eslint:recommended', 'prettier'],
+};
--- generators/app/index.js
+++ generators/app/index.js
@@ -3,12 +3,27 @@ const chalk = require('chalk');
 const yosay = require('yosay');
 const { name } = require('../../package.json');
 
+const CONFLICT_PREFIX = 'conflict';
+const conflictFiles = [`${CONFLICT_PREFIX}.gitignore`];
+const filesToCopy = ['.editorconfig', '.gitattributes', '.prettierrc.js'];
+
 module.exports = class extends Generator {
   prompting() {
     this.log(yosay(`Welcome to the grand ${chalk.red(name)} generator!`));
   }
 
   writing() {
-    this.directory(this.sourceRoot(), this.destinationRoot());
+    conflictFiles.forEach(fileName => {
+      this.fs.copy(
+        this.templatePath(fileName),
+        this.destinationPath(fileName.replace(CONFLICT_PREFIX, ''))
+      );
+    });
+
+    filesToCopy.forEach(fileName => {
+      this.fs.copy(this.templatePath(fileName), this.destinationPath(fileName));
+    });
+
+    // this.fs.copy(this.sourceRoot(), this.destinationRoot());
   }
 };
