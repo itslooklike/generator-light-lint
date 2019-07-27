95fedcfa8e428ee4beffb3eab68d6f3164218133
itslooklike <delsol@bk.ru>
2019-07-27 21:30:31 +0300

    remove prop types

--- generators/app/templates/.eslintrc.js
+++ generators/app/templates/.eslintrc.js
@@ -27,6 +27,7 @@ module.exports = {
   ],
   plugins: ['ramda', 'react-hooks'],
   rules: {
+    'react/prop-types': 0,
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'warn',
   },
