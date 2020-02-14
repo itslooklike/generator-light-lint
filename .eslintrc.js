6461d93021f457c08f6c1d41c7887039ab43ffb5
Alexey Masalov <delsol@bk.ru>
2020-02-14 12:46:13 +0300

    update linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,10 @@
 module.exports = {
+  parser: 'babel-eslint',
   env: {
     node: true,
   },
   parserOptions: {
-    ecmaVersion: 2020,
+    ecmaVersion: 11,
     sourceType: 'module',
   },
   extends: ['eslint:recommended', 'standard', 'prettier'],
--- generators/app/templates/.eslintrc.js
+++ generators/app/templates/.eslintrc.js
@@ -3,31 +3,10 @@ module.exports = {
   env: {
     browser: true,
     node: true,
-    es6: true,
   },
   parserOptions: {
-    ecmaVersion: 2019,
+    ecmaVersion: 11,
     sourceType: 'module',
   },
-  extends: [
-    'eslint:recommended',
-    'standard',
-    'plugin:import/errors',
-    'plugin:import/warnings',
-    'plugin:node/recommended',
-    'plugin:security/recommended',
-    'plugin:promise/recommended',
-    'plugin:react/recommended',
-    'plugin:jsx-a11y/recommended',
-    'plugin:jest/recommended',
-    'plugin:unicorn/recommended',
-    'prettier',
-    'prettier/react',
-  ],
-  rules: {
-    'no-console': 1,
-    'react/prop-types': 0,
-    'unicorn/filename-case': 0,
-    'unicorn/prevent-abbreviations': 0,
-  },
+  extends: ['eslint:recommended', 'standard', 'prettier'],
 }
